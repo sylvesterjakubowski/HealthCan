ff98566e400227ef7a5b280e8df8dc946856a7c5
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-23 12:12:58 -0800

    add link-based authentication to get to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,15 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!, :only => [:dashboard]
+
   def index
-    @users = User.all
+    if current_user
+      redirect_to dashboard_path
+    end
+
   end
 
   def dashboard
+    @user = current_user
 
   end
 end
