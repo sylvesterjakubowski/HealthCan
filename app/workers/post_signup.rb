90741a685bd4738c380baeffd599c78c76de3c23
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-23 15:25:57 -0800

    users starts with 2 appointments

--- app/workers/post_signup.rb
+++ app/workers/post_signup.rb
@@ -11,16 +11,21 @@ class PostSignup
     if Provider.count < 2
       Rake::Task['demo:generate_providers'].invoke
     end
+    @user.approved_providers << Provider.first.id
 
-
+    Appointment.create_oncologist( @user, Provider.first )
+    Appointment.create_labwork( @user, Provider.first )
 
     @message = "Welcome to HealthCan! You can access your dashboard here: #{ApplicationController.get_hostname}#{dashboard_path(:auth_token => @user.authentication_token)}"
 
-    TWILIO_ACCOUNT.sms.messages.create(
-        :from => '+16047575181',
-        :to => user.phone,
-        :body => @message
-    )
+    unless Rails.env.test? || @user.phone.nil?
+      TWILIO_ACCOUNT.sms.messages.create(
+          :from => '+16047575181',
+          :to => user.phone,
+          :body => @message
+      )
+    end
+
 
   end
 end
