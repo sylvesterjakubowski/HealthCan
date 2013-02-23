4818050c8b7a4eeebffaf8970228120cf36b045e
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-22 16:52:19 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthCan::Application.config.secret_token = '24a919c41ec0b56c801e41c123b4bb6b96c7fda87d1207b8a39d933ea07c4e678aa31b723925084d66e8ce50b7ee15b346d039b20825e0712995ebc034eeb69b'
