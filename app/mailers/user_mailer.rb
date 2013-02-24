e476c379148055997f5ee50b7d93a2c2defdb894
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-24 10:15:05 -0800

    email stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,13 @@
 class UserMailer < ActionMailer::Base
   include Resque::Mailer
+  include Rails.application.routes.url_helpers
 
   default from: "HealthCan <no-reply@healthcan.net>"
 
   def welcome_email( user_id )
     @user = User.find( user_id )
 
-    @url  = "http://dashboard.healthcan.net"
-
-    mail(:to => user.email, :subject => "Welcome to HealthCan.net")
-
-
+    mail(:to => @user.email, :subject => "Welcome to HealthCan.net")
   end
 
 end
