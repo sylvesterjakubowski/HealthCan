9cd3d6fc73fbc2c0eae6f997c0973468097ed837
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-24 10:39:12 -0800

    raise deliver errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,14 +14,13 @@ HealthCan::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
 
 
   config.action_mailer.default_url_options = {:host => 'localhost', :port => 3000, :protocol => 'http'}
