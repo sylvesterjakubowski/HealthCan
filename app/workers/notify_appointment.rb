da6863b477f6117973975f0e30b8dd341c74f406
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-24 12:25:45 -0800

    fix apopintment stuff

--- app/workers/notify_appointment.rb
+++ app/workers/notify_appointment.rb
@@ -5,7 +5,7 @@ class NotifyAppointment
 
     @appointment = Appointment.find( appointment_id )
     @user = @appointment.user
-    @message = "#{@appointment.provider.name } has scheduled an appointment for your on #{@appointment.time.to_time.strftime("%Y/%m/%d at %I:%M%p")}. #{ApplicationController.get_hostname}#{Rails.application.routes.url_helpers.dashboard_path(:auth_token => @user.authentication_token)}"
+    @message = "#{@appointment.provider.name } has scheduled an appointment for you on #{@appointment.time.to_time.strftime("%Y/%m/%d at %I:%M%p")}. #{ApplicationController.get_hostname}#{Rails.application.routes.url_helpers.dashboard_path(:auth_token => @user.authentication_token)}"
 
     TWILIO_ACCOUNT.sms.messages.create(
         :from => '+16047575181',
